bae1edb500f30d9054d8342c0a35360b138612f1
Alex Hamilton <alex.hamilton@ou.edu>
2019-01-03 16:55:08 -0600

    Return power_state to cli code.
    There's no need to share it after all.

--- adm/src/parse.rs
+++ adm/src/parse.rs
@@ -1,11 +1,1 @@
 //! Shared utilities for validating/parsing user input.
-
-/// Attempts to parse the (user-passed) string as a target power state.
-pub fn power_state<S: ToString>(s: &S) -> Option<bool> {
-    let lower = s.to_string().to_ascii_lowercase();
-    match lower.as_str() {
-        "on" | "1" => Some(true),
-        "off" | "0" => Some(false),
-        _ => None,
-    }
-}
--- cli/src/turn.rs
+++ cli/src/turn.rs
@@ -1,5 +1,14 @@
 use crate::error::TurnError;
-use adm::{message::Message, parse::power_state};
+use adm::message::Message;
+
+fn power_state<S: ToString>(s: &S) -> Option<bool> {
+    let lower = s.to_string().to_ascii_lowercase();
+    match lower.as_str() {
+        "on" | "1" => Some(true),
+        "off" | "0" => Some(false),
+        _ => None,
+    }
+}
 
 pub fn turn(device: String, state: String) -> Result<Option<Message>, TurnError> {
     power_state(&state)
