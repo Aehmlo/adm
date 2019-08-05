5af683f6598c90e9a4ad2268ca17a6595d5000bb
Alex Hamilton <alex.hamilton@ou.edu>
2019-08-04 22:25:56 -0500

    Split out library along semantic boundaries.
    By having separate modules for input, output, and processing, hopefully
    we can establish nice boundaries and retain orthogonality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,3 @@
-use futures::{Sink, Stream};
-
-/// Encodes information which may be retrieved from an effector.
-pub trait Notification {}
-
-/// A device capable of retrieving information about its environment.
-///
-/// A sensor may be thought of as an asynchronous notification source.
-pub trait Sensor<N>: Stream<Item = N>
-where
-    N: Notification,
-{
-}
-
-/// A command which may be sent to an effector.
-pub trait Message {}
-
-/// A device which can act to change its environment.
-///
-/// A sensor may be thought of as an asynchronous message sink.
-pub trait Effector<M, E>: Sink<SinkItem = M, SinkError = E>
-where
-    M: Message,
-{
-}
+mod sink;
+mod source;
+mod transform;
