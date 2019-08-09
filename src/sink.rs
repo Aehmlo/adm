c117595748a4ed1eff3e7bbc9fc9d265ea8061b9
Alex Hamilton <alex.hamilton@ou.edu>
2019-08-08 20:13:44 -0500

    Update to use nightly Rust.
    This lets us use a newer futures-preview and async/await.

--- src/sink.rs
+++ src/sink.rs
@@ -6,9 +6,9 @@ pub trait Message {}
 /// A device which can act to change its environment.
 ///
 /// An effector may be thought of as an asynchronous message sink.
-pub trait Effector<M>: Sink<SinkItem = M>
+pub trait Effector<M>: Sink<M>
 where
     M: Message,
 {
-    type Error: From<<Self as Sink>::SinkError>;
+    type Error: From<<Self as Sink<M>>::Error>;
 }
